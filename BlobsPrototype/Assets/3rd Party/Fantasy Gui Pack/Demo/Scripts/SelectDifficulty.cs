5207de1b2e2eea5cfd8a62b6ab350d36465ac105
mcdonnellv <vince.mcdonnell@theplayforge.com>
2015-08-26 16:56:15 -0700

    new 3rd party packages

---
+++ BlobsPrototype/Assets/3rd Party/Fantasy Gui Pack/Demo/Scripts/SelectDifficulty.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SelectDifficulty : MonoBehaviour {
+	public int index;
+	public Toggle[] toggles;
+
+	public void OnSelect(){
+		for (int i=0; i< toggles.Length; i++) {
+			if(i != index){
+				toggles[i].isOn=false;
+			}
+		}
+		toggles [index].isOn = true;
+	}
+}
